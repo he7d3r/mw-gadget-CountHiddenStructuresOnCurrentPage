402696a1f22a5d34e9a9ea149d86691fc9ce2191
Helder <he7d3r@gmail.com>
2013-06-13 20:35:05 +0000

    Move the main code inside a function "countHiddenStructures"
    Remove unnecessary variable $

--- src/CountHiddenStructuresOnCurrentPage.js
+++ src/CountHiddenStructuresOnCurrentPage.js
@@ -5,26 +5,29 @@
  */
 /*jslint browser: true, white: true*/
 /*global jQuery, mediaWiki */
-( function ( mw, $ ) {
+( function ( mw ) {
 'use strict';
-	if( mw.config.get( 'wgDBname' ) !== 'ptwiki' || mw.config.get( 'wgAction' ) !== 'view' ){
-		return;
+	function countHiddenStructures (){
+		( new mw.Api() ).get( {
+			action: 'query',
+			titles: mw.config.get( 'wgPageName' ),
+			prop: 'revisions',
+			rvprop: 'content',
+			indexpageids: ''
+		} ).done( function ( data ) {
+			
+			var text = data.query.pages[ data.query.pageids[0] ].revisions[0]['*'],
+				count = text.split( /hiddenStructure|\{\{[Oo]cultar[ _]estrutura/).length - 1;
+			if( count > 0 ){
+				mw.loader.using( 'mediawiki.notify', function(){
+					mw.notify( 'A classe "hiddenStructure" ainda é usada ' + count + ' vezes nesta página.' );
+				} );
+			}
+		} );
 	}
-	( new mw.Api() ).get( {
-		action: 'query',
-		titles: mw.config.get( 'wgPageName' ),
-		prop: 'revisions',
-		rvprop: 'content',
-		indexpageids: ''
-	} ).done( function ( data ) {
-		
-		var text = data.query.pages[ data.query.pageids[0] ].revisions[0]['*'],
-			count = text.split( /hiddenStructure|\{\{[Oo]cultar[ _]estrutura/).length - 1;
-		if( count > 0 ){
-			mw.loader.using( 'mediawiki.notify', function(){
-				mw.notify( 'A classe "hiddenStructure" ainda é usada ' + count + ' vezes nesta página.' );
-			} );
-		}
-	} );
 
-}( mediaWiki, jQuery ) );
+	if( mw.config.get( 'wgDBname' ) === 'ptwiki' || mw.config.get( 'wgAction' ) === 'view' ){
+		mw.loader.using( 'mediawiki.api', countHiddenStructures );
+	}
+
+}( mediaWiki ) );
