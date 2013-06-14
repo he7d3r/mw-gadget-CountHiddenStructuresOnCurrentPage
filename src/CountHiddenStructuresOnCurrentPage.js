e16a843d5f0b71d61a096fbd9d632f7990e2e892
Helder <he7d3r@gmail.com>
2013-06-14 17:58:14 +0000

    Add a column to the table to show the number of uses of the class in each page
    The table is on
    https://pt.wikipedia.org/wiki/WP:Projetos/Padroniza%C3%A7%C3%A3o/hiddenStructure

--- src/CountHiddenStructuresOnCurrentPage.js
+++ src/CountHiddenStructuresOnCurrentPage.js
@@ -3,31 +3,62 @@
  * @author: [[User:Helder.wiki]]
  * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/CountHiddenStructuresOnCurrentPage.js]] ([[File:User:Helder.wiki/Tools/CountHiddenStructuresOnCurrentPage.js]])
  */
-/*jslint browser: true, white: true*/
+/*jslint browser: true, white: true, plusplus: true*/
 /*global jQuery, mediaWiki */
-( function ( mw ) {
+( function ( mw, $ ) {
 'use strict';
 	function countHiddenStructures (){
+		var $table = $( 'table' ),
+			$links = $table.find( 'a' );
+		$table.find( 'tr:first' ).append( '<th>Usos</th>' );
 		( new mw.Api() ).get( {
 			action: 'query',
+			prop: 'links',
+			pllimit: 500,
 			titles: mw.config.get( 'wgPageName' ),
-			prop: 'revisions',
-			rvprop: 'content',
 			indexpageids: ''
 		} ).done( function ( data ) {
-			
-			var text = data.query.pages[ data.query.pageids[0] ].revisions[0]['*'],
-				count = text.split( /hiddenStructure|\{\{[Oo]cultar[ _]estrutura/).length - 1;
-			if( count > 0 ){
-				mw.loader.using( 'mediawiki.notify', function(){
-					mw.notify( 'A classe "hiddenStructure" ainda é usada ' + count + ' vezes nesta página.' );
-				} );
+			var list, i,
+				processBatch,
+				reOldCode = /hiddenStructure|\{\{[Oo]cultar[ _]estrutura/,
+				batchSize = 40;
+			list = $.map( data.query.pages[ data.query.pageids[0] ].links, function( link ){
+				return link.title;
+			} );
+			processBatch = function ( data ) { 
+				var i, page, count, $a, filter;
+				filter = function(){
+					return $( this ).text() === page.title;
+				};
+				for( i = 0; i < data.query.pageids.length; i++ ){
+					page = data.query.pages[ data.query.pageids[i] ];
+					if( page.missing === '' ){
+						count = 0;
+					} else {
+						count = page.revisions[0]['*']
+							.split( reOldCode ).length - 1;
+					}
+					$a = $links.filter( filter );
+					$a.closest( 'tr' ).append( $( '<td>' ).text( count ) );
+				}
+			};
+			for( i = 0; i < list.length; i += batchSize ){
+				( new mw.Api() ).get( {
+					action: 'query',
+					titles: list.slice( i, i + batchSize ).join( '|' ),
+					prop: 'revisions',
+					rvprop: 'content',
+					indexpageids: ''
+				} ).done( processBatch );
 			}
 		} );
 	}
-
-	if( mw.config.get( 'wgDBname' ) === 'ptwiki' || mw.config.get( 'wgAction' ) === 'view' ){
+ 
+	if( mw.config.get( 'wgDBname' ) === 'ptwiki'
+		&& mw.config.get( 'wgAction' ) === 'view'
+		&& mw.config.get( 'wgPageName' ) === 'Wikipédia:Projetos/Padronização/hiddenStructure'
+	){
 		mw.loader.using( 'mediawiki.api', countHiddenStructures );
 	}
-
-}( mediaWiki ) );
+ 
+}( mediaWiki, jQuery ) );
