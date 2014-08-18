503a31384f5bbea17c9c3e2656a1a0b84d5a4e75
Helder <he7d3r@gmail.com>
2014-08-18 18:11:19 -0300

    Inform the license: CC BY-SA 3.0

--- src/CountHiddenStructuresOnCurrentPage.js
+++ src/CountHiddenStructuresOnCurrentPage.js
@@ -1,6 +1,7 @@
 /**
  * Inform the usage of the old "hiddenStructure" class in the pages listed at [[w:WP:Projetos/Padronização/hiddenStructure]]
- * @author: [[User:Helder.wiki]]
+ * @author: Helder (https://github.com/he7d3r)
+ * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/CountHiddenStructuresOnCurrentPage.js]] ([[File:User:Helder.wiki/Tools/CountHiddenStructuresOnCurrentPage.js]])
  */
 /*jshint browser: true, camelcase: true, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, laxbreak: true, maxlen: 120, evil: true, onevar: true */
